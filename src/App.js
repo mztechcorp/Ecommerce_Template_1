f79911ab448665d844c60b6d94c5a08ba6dc44c9
mztechcorp <mztech29@gmail.com>
2020-12-19 11:18:43 -0500

    header updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     //BEM
     <div className="app">
       <Header />
+      <Home />
       {/* Header */}
       {/* Home */}
     </div>
---
+++ src/components/Home/Home.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Home.css";
+import Banner from "../../assets/home-1-plain.jpg";
+import Product from "../Product/Product";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home_container">
+        <img className="home_image" src={Banner} alt="Banner" />
+        <div className="home_row">
+          <Product />
+          <Product />
+          <Product />
+
+          {/* Product */}
+          {/* Product */}
+        </div>
+
+        <div className="home_row">
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
+
+        <div className="home_row">
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+          {/* product */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
